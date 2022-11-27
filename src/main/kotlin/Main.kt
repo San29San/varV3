f5e31f28768ced3f6a9cfca178fab941179d7b23
aleksandr <lodkinsan@gmail.com>
2022-11-27 16:56:16 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    //эта программа считает объём параллелограмма
+   println("Для расчета объёма параллелограмма введите первую цифру")
+    val side1 = readln().toInt()
+    println("Введите вторую цифру")
+    val side2 = readln().toInt()
+    println("Введите третью цифру")
+    val side3 = readln().toInt()
+    val perimeter = side1 * side2 * side3
+    println(perimeter)
+
+}
